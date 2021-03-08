9719af05171f12f2da1da8df4aa7c138e09011a0
Daid <daid303@gmail.com>
2021-03-08 16:21:56 +0100

    Remove valgrind, as we do not have it

--- test/link/test.sh
+++ test/link/test.sh
@@ -101,13 +101,13 @@ for i in smart/*.asm; do
 	startTest
 	$RGBASM -o $otemp $i
 	rm $gbtemp
-	valgrind $RGBLINK -o $gbtemp $otemp
+	$RGBLINK -o $gbtemp $otemp
 	tryCmp "${i%.asm}.bin" $gbtemp
 	rc=$(($? || $rc))
 	rm $gbtemp
 	i="${i%.asm}.smart"
 	startTest
-	valgrind $RGBLINK -vs "root" -o $gbtemp $otemp
+	$RGBLINK -vs "root" -o $gbtemp $otemp
 	tryCmp "$i.bin" $gbtemp
 	rc=$(($? || $rc))
 done
