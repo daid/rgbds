749bfff6e3e678d2f064348dc6b95fbc5efb23c5
Daid <daid303@gmail.com>
2021-03-08 11:03:08 +0100

    Fix some smart linking tests and remove some obviously broken/wrong ones. Still needs more tests!

--- test/link/test.sh
+++ test/link/test.sh
@@ -100,11 +100,14 @@ done
 for i in smart/*.asm; do
 	startTest
 	$RGBASM -o $otemp $i
+	rm $gbtemp
 	$RGBLINK -x -o $gbtemp $otemp
 	tryCmp "${i%.asm}.bin" $gbtemp
 	rc=$(($? || $rc))
-	$RGBLINK -x -vs "root" -o $gbtemp $otemp
+	rm $gbtemp
 	i="${i%.asm}.smart"
+	startTest
+	$RGBLINK -x -vs "root" -o $gbtemp $otemp
 	tryCmp "$i.bin" $gbtemp
 	rc=$(($? || $rc))
 done
